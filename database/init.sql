39d413b3107c6386816d0e2417335189b8b03982
Pedro Juan <pedrosaraivaofc@gmail.com>
2026-05-22 16:49:43 -0300

    chore(infra): add Dockerfile, docker-compose, and database init script

---
+++ database/init.sql
@@ -0,0 +1,29 @@
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE tags (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE articles (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT,
+    category_id INT REFERENCES categories(id),
+    author_id INT REFERENCES users(id)
+);
+
+CREATE TABLE article_tags(
+    article_id INT REFERENCES articles(id),
+    tag_id INT REFERENCES tags(id)
+);
